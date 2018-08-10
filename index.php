7f82bc6575922b87237b8bd15df28b0688f4ed3b
Caleb Ventura Vasquez <caleb.ventura.v@uni.pe>
2018-08-10 11:13:43 -0500

    Barra de navegacion
    Esta es la version 1 de la barra de navegacion. se ha cambiado código de bootstrap, ademas de agregado estilo propio.

--- index.php
+++ index.php
@@ -13,20 +13,54 @@
   <link rel="apple-touch-icon" href="icon.png">
 
   <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
 
-  <div class="cu-header"></div>
+  <div class="cu-header">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light ">
+        <a class="navbar-brand" href="index.php">
+          <img src="img/comsoc.png" width="100px" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav m-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">INICIO</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">EXPERIENCIAS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">GALERIA</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">NOSOTROS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">CONTACTANOS</a>
+            </li>
+
+          </ul>
+        </div>
+      </nav>      
+    </div>
+
+  </div>
   <div class="cu-main"></div>
   <div class="cu-footer"></div>
 
   <script src="js/vendor/modernizr-3.6.0.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+
 
   <script src="js/vendor/jquery-3.3.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
   <script src="js/plugins.js"></script>
   <script src="js/main.js"></script>
 
