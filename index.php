be3c1f41fb8dc5c2b68fd2bc8e8ebe428776d8fc
Caleb Ventura Vasquez <caleb.ventura.v@uni.pe>
2018-08-15 00:55:02 -0500

    seccion sobre equipo
    Seccion sobre el equipo terminado

--- index.php
+++ index.php
@@ -14,47 +14,115 @@
 
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
   <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
+  <header>
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light ">
+          <a class="navbar-brand" href="index.php">
+            <img src="img/comsoc.png" width="100px" alt="">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-  <div class="cu-header">
-    <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-light ">
-        <a class="navbar-brand" href="index.php">
-          <img src="img/comsoc.png" width="100px" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav m-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">INICIO</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">EXPERIENCIAS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">GALERIA</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">NOSOTROS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">CONTACTANOS</a>
+              </li>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav m-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">INICIO</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">EXPERIENCIAS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">GALERIA</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">NOSOTROS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">CONTACTANOS</a>
-            </li>
+            </ul>
+          </div>
+        </nav>      
+      </div>
+  </header>
+  <main>
 
-          </ul>
+    <!-- PORTADA -->
+    <section>
+      
+    </section>
+
+    <!-- SOBRE EL PROYECTO -->
+    <section>
+      
+    </section>
+
+
+    <!-- CARACTERISTICAS -->
+    <section class="my-5">
+      <div>
+        <h2 class="text-center mb-5">Mejores características</h2>        
+      </div>
+      <div class="caracteristicas py-5">
+        <div class="container">
+          <div class="row sobre-equipo">
+              <div class="col-sm-3 text-center">
+                  <i class="fas fa-wifi"></i>
+                  <h3 class="text-uppercase my-4">Conectividad inmediata</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore atque, libero ex fugiat asperiores itaque, reprehenderit nihil amet maiores hic perferendis, doloribus aut?</p>
+              </div>
+              <div class="col-sm-3 text-center">
+                  <i class="fas fa-mobile"></i>
+                  <h3 class="text-uppercase my-4">Compatibilidad con dispositivos</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste at debitis minus, sint natus velit, quia tempora dolores, ratione esse aliquid facilis!</p>
+              </div>
+              <div class="col-sm-3 text-center">
+                  <i class="fas fa-check"></i>
+                  <h3 class="text-uppercase my-4">Medida eficiente</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, possimus hic aspernatur error quod libero quo consequatur minus unde. Inventore architecto odio quasi sequi.</p>
+              </div>
+              <div class="col-sm-3 text-center">
+                  <i class="fas fa-rocket"></i>
+                  <h3 class="text-uppercase my-4">Rápidos reportes</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum obcaecati incidunt, id, eum minus mollitia assumenda excepturi illo, dolore at modi iste.</p>
+              </div>
+          </div>
         </div>
-      </nav>      
-    </div>
+      </div>
+    </section>
 
-  </div>
-  <div class="cu-main"></div>
-  <div class="cu-footer"></div>
+    <!-- MIRALO EN ACCION -->
+    <section>
+      
+    </section>
+
+    <!-- APLICACIONES -->
+    <section>
+      
+    </section>
+
+    <!-- NUESTROS PRODUCTOS -->
+    <section>
+      
+    </section>
+
+    <!-- NOSOTROS -->
+    <section>
+      
+    </section>
+
+  </main>
+
+  <footer>
+    
+  </footer>
 
   <script src="js/vendor/modernizr-3.6.0.min.js"></script>
 
